3e928219fc3bfb8a984a5b7b9e21d4bcb364b72b
Mahdi Abbaspour Shahmarasi <mahdi.abbaspour.sh@gmail.com>
2024-04-20 00:32:54 +0430

    boostrap added to project

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
-const {app, BrowserWindow} = require('electron') 
+const {app, BrowserWindow, Menu} = require('electron') 
 const createWindow=()=>
 {
     let win= new BrowserWindow({
-        width: 800, 
-        height: 600,
-        title:'مدیریت کیف پول من'
+        width: 900, 
+        height: 700,
+        title:'مدیریت کیف پول من',
+        //frame:false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+          },
     })   
     win.loadFile('index.html');
-    win.webContents.openDevTools();
+    //win.webContents.openDevTools();
+
+    const template=[
+        {
+            label: 'فایل',
+            submenu:[
+                {label:'ساخت فایل پایگاه داده جدید',},    
+                {label:'بازکردن فایل پایگاه داده',},                
+                {
+                    label:'فعال سازی حالت برنامه نویسی',
+                    click()
+                    {
+                        win.webContents.openDevTools();
+                    }
+                },
+                {label:'خروج',},
+            ]
+        }
+    ];
+    const myMenu=Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(null);
+
 }
 app.on('ready', createWindow);
